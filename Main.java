a2009599c796e3b8f775cd0cbf0a6743975863fe
flaxoDS <84013197+flaxoDS@users.noreply.github.com>
2023-01-08 16:30:20 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Code at line 4.");
         System.out.println("Code at line 5 is now changed in branch main.");
-        System.out.println("Some changes are made in line 6 alala");
+        System.out.println("Some changes are made in line 6ewgerhrtn alala");
     }
 }
