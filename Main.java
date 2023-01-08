024947d9cce53a9dad1af2dd40ece154bcb67b69
FlaxoDS <Felix@Gimkiewicz.de>
2023-01-08 16:22:35 +0100

    main added

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("Code at line 4.");
+        System.out.println("Code at line 5 is now changed in branch main.");
+        System.out.println("Some changes are made in line 6");
+    }
+}
