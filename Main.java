7ad7f505904f71b85c7b382946b126fc9e06f62f
flaxoDS <84013197+flaxoDS@users.noreply.github.com>
2025-05-15 12:55:16 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-
-    public static void main(String[] args){
-        System.out.println("Code at line 4.");
-        System.out.println("Code at line 5 is now changed in branch main.");
-        System.out.println("Some changes are made in line 6ewgerhrtn alala");
-    }
-}
